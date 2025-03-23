f95cf374dda2a160a4e69404bde3389747ce3856
Sofyan Wanadi <sofyanwanadi6@gmail.com>
2025-03-24 01:56:50 +0700

    github workflows

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/myWeb-v2",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
